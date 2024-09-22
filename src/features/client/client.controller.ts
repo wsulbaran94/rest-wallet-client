f59458b2c52415aba050b986a5a5e975b08bd45a
Wilfredo Sulbaran <wilfredosulbaran94@gmail.com>
2024-09-22 14:34:12 -0500

    feature: add documents

--- src/features/client/client.controller.ts
+++ src/features/client/client.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ResponseDto } from 'src/common/dto/response.dto';
 
 @ApiTags('clientes')
 @Controller('client')
@@ -9,7 +10,11 @@ export class ClientController {
   constructor(private readonly clientService: ClientService) {}
 
   @Post()
-  @ApiResponse({ status: 201, description: 'Cliente creado.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Cliente creado.',
+    type: ResponseDto,
+  })
   create(@Body() createClientDto: CreateClientDto) {
     return this.clientService.create(createClientDto);
   }
